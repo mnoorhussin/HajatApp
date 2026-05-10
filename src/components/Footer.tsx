80fd054ca3899d1d5223adbd90571b12b082c31a
Mustafa <mu88ali@gmail.com>
2026-05-10 14:48:55 +0200

    feat: add Footer component with navigation, contact info, and social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,18 @@
-import { Facebook, Twitter, Instagram, Mail, MapPin } from 'lucide-react';
+import { Facebook, Instagram, Mail, MapPin } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+
+const TiktokIcon = ({ size = 24 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
+  </svg>
+);
 
 export default function Footer() {
   return (
@@ -58,9 +70,9 @@ export default function Footer() {
           
           <div className="flex items-center gap-4">
             {[
-              { icon: Facebook, href: 'https://facebook.com/hajatapp' },
-              { icon: Twitter, href: 'https://x.com/hajatapp' },
-              { icon: Instagram, href: 'https://instagram.com/hajatapp' }
+              { icon: Facebook, href: 'https://www.facebook.com/profile.php?id=61589581559138' },
+              { icon: TiktokIcon, href: 'https://www.tiktok.com/@hajatapp' },
+              { icon: Instagram, href: 'https://www.instagram.com/hajatapp/' }
             ].map((item, i) => (
               <a 
                 key={i} 
